ae001dbff22d86460ece2a0fc01ee1ce23be6e50
subratadebnath2507 <subratadebnath2507>
2025-10-13 20:17:48 +0530

    commit done for the page header

---
+++ UI/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+
+
+
+@NgModule({
+  declarations: [
+    PageHeaderComponent
+  ],
+  imports: [CommonModule, MaterialModule],
+  exports: [CommonModule, MaterialModule, PageHeaderComponent],
+})
+export class SharedModule { }
