005d520c3d449b86849384ff80fda3b41821a794
subratadebnath2507 <subratadebnath2507>
2025-11-12 19:54:05 +0530

    main changes

---
+++ API/API/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+    }
+}
