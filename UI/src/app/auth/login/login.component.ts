7f869fc6732869fbf5f10a568524f128e4632ec3
subratadebnath2507 <subratadebnath2507>
2025-11-12 19:52:44 +0530

    Added Login Form

--- UI/src/app/auth/login/login.component.ts
+++ UI/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';  
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'login',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  hidePassword:boolean = true;
 
+  constructor(fb:FormBuilder){
+    this.loginForm = fb.group({
+      email:fb.control('', [Validators.required]),
+      password:fb.control('', [Validators.required])
+    });
+  }
+
+  login(){
+    let user = {
+      email: this.loginForm.value.get("email")?.value,
+      password: this.loginForm.value.get("password")?.value
+    }
+  } 
 }
